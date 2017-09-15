705ef4db8b445956787283110fc3e97abfe6af19
wangyw15 <wangyanwei2012@sina.com>
2017-09-15 21:00:38 +0800

    Add startup method and add public variables

---
+++ LBTL/Global/Variable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KMCCC.Launcher;
+
+namespace LBTL.Global
+{
+    public static class Variable
+    {
+        public static LauncherCore Core = null;
+    }
+}
